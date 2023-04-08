0fe26683c80f75f84e285325638b616528d8635b
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2023-04-08 15:03:28 -0300

    ProjectForms and Projects CRUD

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(request):
@@ -11,3 +12,38 @@ def project(request, pk):
     project = Project.objects.get(id=pk)
     tags = project.tags.all()
     return render(request, 'projects/single-projects.html', {'project': project, 'tags': tags})
+
+
+def create_project(request):
+    if request.method == 'GET':
+        form = ProjectForm()
+        context = {'forms': form}
+        return render(request, 'projects/project_form.html', context)
+    elif request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {'forms': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'GET':
+        return render(request, 'projects/delete_template.html', {'project': project})
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
